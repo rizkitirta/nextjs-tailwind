09a110de9165e2ff99fe35311c0efe500c3011f1
rizkitirta <rizkibuwono0@gmail.com>
2022-04-14 22:42:06 +0700

    about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,13 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: '#f59e0b',
-        secondary: '#f97316',
-        info: '#10b981',
-        infoLight: '#06b6d4',
-        dark: '#334155'
+        'primary': '#f59e0b',
+        'secondary': '#f97316',
+        'info': '#10b981',
+        'info-light': '#06b6d4',
+        'dark': '#334155',
+        'dark-light': '#64748b',
+
       },
       screens:{
         '2xl': '1320px'
